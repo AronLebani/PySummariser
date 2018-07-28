6c93a3a31cfc980e2b0e43a1fa98307107decd3d
Aron Lebani <aron.lebani@gmail.com>
2018-07-28 14:37:44 +1000

    Add test script

---
+++ test.py
@@ -0,0 +1,17 @@
+from summariser import Summariser
+
+def testSummariser():
+    input = "Classifier4J is a java package for working with text. Classifier4J includes a summariser."
+    expectedResult = "Classifier4J is a java package for working with text."
+
+    summariser = Summariser()
+    result = summariser.summarise(input, 1)
+    assert(expectedResult == result);
+
+    input = "Classifier4J is a java package for working with text. Classifier4J includes a summariser. A Summariser allows the summary of text. A Summariser is really cool. I don't think there are any other java summarisers.";
+    expectedResult = "Classifier4J is a java package for working with text. Classifier4J includes a summariser.";
+    result = summariser.summarise(input, 2);
+    assert(expectedResult == result);
+
+if name == __main__:
+    testSummariser()
